fc79d18674e955bbe8d31e8c9eef04eae5112840
Shadowed <shadowed.wow@gmail.com>
2009-12-11 14:33:33 -0800

    Added .pkgmeta, Libs/ structure  and the TOC setup

--- core.lua
+++ core.lua
@@ -1,4 +1,4 @@
-GroupHistory = LibStub("AceAddon-3.0"):NewAddon("GroupHistory", "AceEvent-3.0", "AceConsole-3.0", "AceHook-3.0", "AceTimer-3.0")
+GroupHistory = LibStub("AceAddon-3.0"):NewAddon("GroupHistory", "AceEvent-3.0", "AceTimer-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("SexyGroup")
 
 local defaults = {}
