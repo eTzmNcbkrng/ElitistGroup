26f12851d986d5adc3abcefd5c22e4552fa7499a
Shadowed <shadowed.wow@gmail.com>
2009-12-24 14:01:10 -0800

    Added scan file that will do the actual saving/scanning of players

---
+++ scan.lua
@@ -0,0 +1,9 @@
+local SexyGroup = select(2, ...)
+local Scan = SexyGroup:NewModule("Scan", "AceEvent-3.0")
+
+local L = LibStub("AceLocale-3.0"):GetLocale("SexyGroup")
+
+function Scan:UpdatePlayerData()
+
+end
+
