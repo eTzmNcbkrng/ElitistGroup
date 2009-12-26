85a774f4e1abb828299237cc9b4959639ccfd6df
Shadowed <shadowed.wow@gmail.com>
2009-12-25 19:47:01 -0800

    Cleaned up the group rating frames so they will work with the specific database format we are using Fixed UIObjects being held by the rating frame rather than releasing them all into the pool when the frames closed Data should be saved by group now, meaning you can set a rating/notes and open it up later before joining another group and you will see the comments you had set

--- group_history.lua
+++ group_history.lua
@@ -1,152 +1,169 @@
 local SexyGroup = select(2, ...)
+local History = SexyGroup:NewModule("GroupHistory", "AceEvent-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("SexyGroup")
 local AceGUI = LibStub("AceGUI-3.0")
-local parent = SexyGroup
-local mod = parent:NewModule("GroupHistory", "AceEvent-3.0")
-local surveyFrame
-local SpecialFrame = CreateFrame("Frame", "SexyGroupHistoryHider")
-SpecialFrame:SetScript("OnHide", function()
-	surveyFrame:Hide()
-end)
-local defaults = {
-	global = {
-		characters = {}
-	}
-}
-local chars
+local surveyFrame, SpecialFrame
+local groupRatings, groupNotes, groupList = {}, {}, {}
 
-mod.lastRun = {}
-function mod:OnInitialize()
-	self.db = parent.db:RegisterNamespace("GroupHistory", defaults)
-	chars = self.db.global.characters
-	f = AceGUI:Create("Frame")	
-	tinsert(UISpecialFrames,"SexyGroupHistoryHider")
-	surveyFrame = f
-	f:SetTitle(L["Rate This Group"])
-	f:SetStatusText("")
-	f:SetLayout("Flow")
-	f:SetWidth(495)
-	f:SetHeight(395)
-	f:Hide()
+function History:LFG_COMPLETION_REWARD()
+	if( SexyGroup.db.profile.autoPopup ) then
+		self:LogGroup()
+	else
+		local name, typeID = GetLFGCompletionReward()
+		if( typeID == TYPEID_HEROIC_DIFFICULTY ) then
+			name = string.format(HEROIC_PREFIX, name)
+		end
+		
+		SexyGroup:Print(string.format(L["Completed %s! Type /rate to rate this group."], name))
+	end
+end
+
+function History:OnInitialize()
+	self:RegisterEvent("LFG_COMPLETION_REWARD")
+
+	SpecialFrame = CreateFrame("Frame", "SexyGroupHistoryHider")
+	SpecialFrame:SetScript("OnHide", function()
+		if( surveyFrame ) then
+			surveyFrame:Hide()
+		end
+	end)
+
+	table.insert(UISpecialFrames, "SexyGroupHistoryHider")
 	
 	SLASH_SEXYGROUPRATE1 = "/rate"
 	SlashCmdList["SEXYGROUPRATE"] = function(msg, editbox)
-		mod:LogGroup()
+		if( GetNumPartyMembers() == 0 and not self.activeGroupID ) then
+			SexyGroup:Print(L["You need to currently be in a group, or have been in a group to use the rating tool."])
+			return
+		end
+		
+		History:LogGroup()
 	end	
 end
 
-function mod:InitFrame()
-	local f = surveyFrame
-	surveyFrame:ReleaseChildren()
+local function OnEnterPressed(self, event, text)
+	groupNotes[self:GetUserData("partyID")] = text
+end
+
+local function OnValueChanged(self, event, value)
+	groupRatings[self:GetUserData("partyID")] = value
+end
+			
+local function OnHide(self)
+	for partyID, role in pairs(groupList) do
+		if( groupRatings[partyID] and groupNotes[partyID] ) then
+			local userData = SexyGroup.userData[partyID]
+			local note
+			for i=1, #(userData.notes) do
+				if( userData.notes[i].from == SexyGroup.playerName ) then
+					note = userData.notes[i]
+					table.remove(userData.notes, i)
+					break
+				end
+			end
+			
+			note = note or {}
+			note.from = SexyGroup.playerName
+			note.role = note.role and bit.bor(note.role, role) or role
+			note.rating = groupRatings[partyID]
+			note.comment = groupNotes[partyID]
+			note.time = time()
+			table.insert(userData.notes, note)
+		end
+	end
+	
+	surveyFrame = nil
+	AceGUI:Release(self)
+end
+
+function History:InitFrame()
+	surveyFrame = AceGUI:Create("Frame")	
+	surveyFrame:SetCallback("OnClose", OnHide)
+	surveyFrame:SetTitle(L["Rate This Group"])
+	surveyFrame:SetStatusText("")
+	surveyFrame:SetLayout("Flow")
+	surveyFrame:SetWidth(495)
+	surveyFrame:SetHeight(245 + (GetNumPartyMembers() > 2 and 155 or 0))
+	surveyFrame:Show()
+
+	-- Be do be do be dooooo
+	SpecialFrame:Show()
+	PlaySoundFile([[Interface\AddOns\SexyGroup\question.mp3]])
 
 	local header = AceGUI:Create("Heading")
 	header:SetText(L["Rate and make notes on the players in your group."])
 	header.width = "fill"
-	f:AddChild(header)
+	surveyFrame:AddChild(header)
 	
-	for i = 1, 4 do
-		-- local key = "player"
-		local key = "party" .. i
-		if UnitExists(key) then
-			local isTank, isHealer, isDamage = UnitGroupRolesAssigned(key)
-			local role = (isTank and TANK) or (isHealer and HEALER) or (isDamage and DAMAGE) or ""
-			local classToken = select(2, UnitClass(key))
-			local g = AceGUI:Create("InlineGroup")
-			g:SetWidth(230)			
-			
-			local label = AceGUI:Create("Label")
-			label:SetWidth(230)
-			local name = UnitName(key)
-			label:SetText(role ~= "" and (name .. " - " .. role) or name)
-			if( classToken ) then
-				label:SetImage("Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Classes", CLASS_ICON_TCOORDS[classToken][1], CLASS_ICON_TCOORDS[classToken][2], CLASS_ICON_TCOORDS[classToken][3], CLASS_ICON_TCOORDS[classToken][4])
-				label:SetImageSize(24, 24)
-			else
-				label:SetImage("")
-			end	
-			label:SetFontObject(SystemFont_Huge1)
-			g:AddChild(label)
-			
-			local rating = AceGUI:Create("Slider")
-			rating:SetSliderValues(0, SexyGroup.MAX_RATING, 1)
-			rating:SetValue(3)
-			-- rating:SetLabel(L["Rate this player's performance"])
-			rating:SetCallback("OnValueChanged", function(self, event, value)
-				if not mod.lastRun[UnitGUID(key)] then return end
-				mod.lastRun[UnitGUID(key)].rating = value
-			end)
-			rating.lowtext:SetText(L["Terrible"])
-			rating.hightext:SetText(L["Great"])
-			g:AddChild(rating)
+	for i = 1, GetNumPartyMembers() do
+		local unit = "party" .. i
+		local isTank, isHealer, isDamage = UnitGroupRolesAssigned(unit)
+		local role = (isTank and TANK) or (isHealer and HEALER) or (isDamage and DAMAGE) or ""
+		local classToken = select(2, UnitClass(unit))
+		local name, server = UnitName(unit)
+		local partyID = string.format("%s-%s", name, server and server ~= "" and server or GetRealmName())
 
-			local notes = AceGUI:Create("EditBox")
-			notes:SetLabel(L["Notes"])
-			notes:SetCallback("OnEnterPressed", function(self, event, text)
-				if not mod.lastRun[UnitGUID(key)] then return end
-				mod.lastRun[UnitGUID(key)].notes = text
-			end)
-			g:AddChild(notes)
-			
-			f:AddChild(g)
-		end
+		local group = AceGUI:Create("InlineGroup")
+		group:SetWidth(230)			
+		
+		local label = AceGUI:Create("Label")
+		label:SetWidth(300)
+		label:SetText(name)
+		if( classToken ) then
+			label:SetImage("Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Classes", CLASS_ICON_TCOORDS[classToken][1], CLASS_ICON_TCOORDS[classToken][2], CLASS_ICON_TCOORDS[classToken][3], CLASS_ICON_TCOORDS[classToken][4])
+			label:SetImageSize(24, 24)
+		else
+			label:SetImage("")
+		end	
+		label:SetFontObject(SystemFont_Huge1)
+		group:AddChild(label)
+		
+		local rating = AceGUI:Create("Slider")
+		rating:SetSliderValues(1, SexyGroup.MAX_RATING, 1)
+		rating:SetValue(groupRatings[partyID] or 3)
+		rating:SetCallback("OnValueChanged", OnValueChanged)
+		rating:SetUserData("partyID", partyID)
+		rating.lowtext:SetText(L["Terrible"])
+		rating.hightext:SetText(L["Great"])
+		group:AddChild(rating)
+
+		local notes = AceGUI:Create("EditBox")
+		notes:SetLabel(L["Notes"])
+		notes:SetText(groupNotes[partyID] or string.format("%s - ", role))
+		notes:SetCallback("OnEnterPressed", OnEnterPressed)
+		notes:SetUserData("partyID", partyID)
+		group:AddChild(notes)
+		
+		surveyFrame:AddChild(group)
 	end
-	f:SetStatusText((L["Instance run: %s"]):format(
+	
+	surveyFrame:SetStatusText((L["Instance run: %s"]):format(
 		GetInstanceDifficulty() > 1 and (HEROIC_PREFIX):format(GetRealZoneText()) or GetRealZoneText()
 	))
-	self:ShowFrame()
 end
 
-function mod:ShowFrame()
-	surveyFrame:Show()
-	-- surveyFrame:SetAlpha(0)
-	SpecialFrame:Show()
-	PlaySoundFile([[Interface\AddOns\SexyGroup\question.mp3]])
-end
-
-function mod:OnEnable()
-	self:RegisterEvent("LFG_COMPLETION_REWARD")
-end
-
-local guids = {}
-function mod:BuildGroupKey()
-	wipe(guids)
-	for i = 1, 4 do
-		if UnitExists("party" .. i) then
-			tinsert(guids, UnitGUID("party" .. i))
+function History:LogGroup()
+	local groupID = ""
+	for i=1, GetNumPartyMembers() do groupID = groupID .. UnitGUID("party" .. i) end
+	if( groupID == "" and not self.activeGroupID ) then return end
+	
+	if( groupID ~= "" and ( not self.activeGroupID or self.activeGroupID ~= groupID ) ) then
+		table.wipe(groupList)
+		table.wipe(groupNotes)
+		table.wipe(groupRatings)
+		
+		for i=1, GetNumPartyMembers() do
+			SexyGroup.modules.Scan:CreateCoreTable("party" .. i)
+			
+			local name, server = UnitName("party" .. i)
+			local partyID = string.format("%s-%s", name, server and server ~= "" and server or GetRealmName())
+			local isTank, isHealer, isDamage = UnitGroupRolesAssigned(unit)
+			groupList[partyID] = bit.bor(isTank and SexyGroup.ROLE_TANK or 0, isHealer and SexyGroup.ROLE_HEALER or 0, isDamage and SexyGroup.ROLE_DAMAGE or 0)
 		end
+
+		self.activeGroupID = groupID
 	end
-	return table.concat(guids)
+	
+	self:InitFrame()
 end
 
-function mod:LogGroup()
-	local groupKey = self:BuildGroupKey()
-	if mod.groupKey ~= groupKey then
-		mod.groupKey = groupKey
-		for i = 1, 4 do
-			local key = "party" .. i
-			if UnitName(key) and not UnitIsUnit(key, "player") then
-				local name = UnitName(key)
-				local guid = UnitGUID(key)
-				chars[guid] = chars[guid] or {}
-				local isTank, isHealer, isDamage = UnitGroupRolesAssigned(key)
-				local run = {
-					role = bit.bor(isTank and SexyGroup.ROLE_TANK or 0, isHealer and SexyGroup.ROLE_HEALER or 0, isDamage and SexyGroup.ROLE_DAMAGE or 0),
-					instance = GetRealZoneText(),
-					notes = "",
-					rating = 0
-				}
-				tinsert(chars[guid], run)
-				mod.lastRun[guid] = run			
-			end
-		end
-		self:InitFrame()
-	else
-		self:ShowFrame()
-	end
-end
-
--- Dungeon complete, popup survey
-function mod:LFG_COMPLETION_REWARD()
-	wipe(mod.lastRun)
-	self:LogGroup()
-end
