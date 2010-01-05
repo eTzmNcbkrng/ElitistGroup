b01c3a3b643f464f411d76c5055aa143e817e45a
Shadowed <shadowed.wow@gmail.com>
2010-01-05 13:37:03 -0800

    Tweaked flags for localization a bit more

--- localization/deDE.lua
+++ localization/deDE.lua
@@ -1,5 +1,5 @@
 if( GetLocale() ~= "deDE" ) then return end
 local L = {}
---@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="ignore")@
+--@localization(locale="deDE", format="lua_additive_table", escape-non-ascii=true, handle-unlocalized="ignore")@
 local ElitistGroup = select(2, ...)
 ElitistGroup.L = setmetatable(L, {__index = ElitistGroup.L})
--- localization/frFR.lua
+++ localization/frFR.lua
@@ -1,5 +1,5 @@
 if( GetLocale() ~= "frFR" ) then return end
 local L = {}
---@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="ignore")@
+--@localization(locale="frFR", format="lua_additive_table", escape-non-ascii=true, handle-unlocalized="ignore")@
 local ElitistGroup = select(2, ...)
 ElitistGroup.L = setmetatable(L, {__index = ElitistGroup.L})
