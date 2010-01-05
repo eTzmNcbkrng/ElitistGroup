6921711177e380be46103cf6bdeddcb9ee15091d
Shadowed <shadowed.wow@gmail.com>
2010-01-05 13:53:05 -0800

    More localization file tweaking

--- localization/zhCN.lua
+++ localization/zhCN.lua
@@ -1,5 +1,5 @@
 if( GetLocale() ~= "zhCN" ) then return end
 local L = {}
---@localization(locale="zhCN", format="lua_additive_table", escape-non-ascii=true, handle-unlocalized="ignore")@
+--@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="ignore")@
 local ElitistGroup = select(2, ...)
 ElitistGroup.L = setmetatable(L, {__index = ElitistGroup.L})
--- localization/zhTW.lua
+++ localization/zhTW.lua
@@ -1,5 +1,5 @@
 if( GetLocale() ~= "zhTW" ) then return end
 local L = {}
---@localization(locale="zhTW", format="lua_additive_table", escape-non-ascii=true, handle-unlocalized="ignore")@
+--@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized="ignore")@
 local ElitistGroup = select(2, ...)
 ElitistGroup.L = setmetatable(L, {__index = ElitistGroup.L})
