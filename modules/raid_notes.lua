d4cbf6b9d0ec94bd6e1abd6a8cb92f108926c11c
Shadowed <shadowed.wow@gmail.com>
2010-01-08 16:21:56 -0800

    Added raid notes file just so there are no errors

---
+++ modules/raid_notes.lua
@@ -0,0 +1,15 @@
+local ElitistGroup = select(2, ...)
+local History = ElitistGroup:NewModule("RaidHistory", "AceEvent-3.0")
+local L = ElitistGroup.L
+
+--[[
+-- 1 = inv_jewelcrafting_gem_37
+-- 2 = inv_jewelcrafting_gem_39
+-- 3 = inv_jewelcrafting_dragonseye03
+-- 4 = inv_jewelcrafting_gem_42
+-- 5 = inv_jewelcrafting_gem_41
+
+
+
+]]
+
